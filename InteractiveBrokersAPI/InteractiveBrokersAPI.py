df49966bc672053d0146a17a2447898439ba97fc
Jan van Bergen <39480577+CacheMoneyPlaya@users.noreply.github.com>
2020-09-12 23:49:27 +0100

    use dataframes for storing positions and orders

--- InteractiveBrokersAPI/InteractiveBrokersAPI.py
+++ InteractiveBrokersAPI/InteractiveBrokersAPI.py
@@ -5,36 +5,36 @@ from ibapi.order import *
 import threading
 import time
 from overrides import overrides
+import pandas as pd
 
-
-#Orders
-#Positions
-#Current liquidity
-
+#Orders -> DONE
+#Positions -> DONE
+#Current liquidity -> DONE
 
 class InteractiveBrokersAPI(EWrapper, EClient):
 
     equity = None
-    orders = list()
-    positions = list()
 
     def __init__(self):
          EClient.__init__(self, self)
          self.nextOrderId = None
+         self.positions = pd.DataFrame([], columns = ['Symbol', 'Quantity'])
+         self.orders = pd.DataFrame([], columns = ['Symbol', 'Quantity', 'Action'])
 
     def start_threading(self):
         api_thread = threading.Thread(target=run_loop, daemon=True)
         api_thread.start()
         time.sleep(1)
 
+    def reset_data_frame(self, dataframe):
+        dataframe.index = self.orders.index + 1
+        return dataframe.sort_index()
+
     @overrides
     def openOrder(self, orderId, contract, order, orderState):
         super().openOrder(orderId, contract, order, orderState)
-        self.orders.append({
-            'sym' : contract.symbol,
-            'qty' : order.totalQuantity,
-            'act' : order.action
-        })
+        self.orders.loc[-1] = [contract.symbol, order.totalQuantity, order.action]
+        self.orders = self.reset_data_frame(self.orders)
 
     @overrides
     def nextValidId(self, orderId):
@@ -45,10 +45,9 @@ class InteractiveBrokersAPI(EWrapper, EClient):
     def position(self, account, contract, position, avgCost):
         super().openOrder(account, contract, position, avgCost)
         if account == 'DU2650196':
-            self.positions.append({
-                'sym': contract.symbol,
-                'qty': position
-            })
+            self.positions.loc[-1] = [contract.symbol, position]
+            self.positions = self.reset_data_frame(self.positions)
+
 
     def get_next_id(self, num):
         return EClient.reqIds(num)
@@ -98,12 +97,10 @@ api_thread.start()
 app.reqPositions()
 time.sleep(1)
 print(app.positions)
-# print(app.orders)
-# print(app.orders)
 # app.reqAccountSummary(101, "All", "AvailableFunds")
 # time.sleep(3) #Sleep interval to allow time for connection to server
 #
-# #Place order
+#Place order
 # aapl_contract = app.create_contract('AAPL')
 # aapl_order = app.create_order(10, 'BUY')
 # app.placeOrder(app.nextOrderId, aapl_contract, aapl_order)
